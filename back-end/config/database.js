f3c31d21af02d4967a38fb6fbc352e32edfdf9d1
Wellington2212 <wellingtonwcmoreira@gmail.com>
2020-09-01 01:25:50 +0000

    (2020/08/31) Termino da configuração da conexão de BD e primeiro model

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -5,4 +5,21 @@ module.exports = uri => {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
+
+    mongoose.connection.on('connected', () =>
+        console.log('==> Mongoose! conectado com sucesso ao servidor')
+    )
+
+    mongoose.connection.on('disconnected', () =>
+        console.log('==> Mongoose! desconectado do servidor')
+    )
+
+    //capturamos um sinal de encerramento (SIGINT), ctrl + c
+    process.on('SIGINT', () =>
+        mongoose.connection.close(() => {
+            console.log('==> Mongoose! Desconectado pelo termino da aplicação')
+            //0 indica que a finalização ocorreu sem erros
+            process.exit(0);
+        })
+    )
 }
---
+++ back-end/models/Curso.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+const esquema = mongoose.Schema({
+    nome: {
+        type: String,
+        required:true
+    },
+    ementa: {
+        type: String,
+        required: true
+    },
+    carga_horaria: {
+        type: Number,
+        required: true,
+        min: 4,
+        max: 80
+    },
+    nivel: {
+        type: String,
+        required: true,
+        enum:['Básico', 'Intermediário', 'Avançado']
+    },
+    valor_curso:{
+        type: Number,
+        required: true,
+        default: 450, //valor assumido se não for informado
+        min: 50
+    }
+})
+
+/*
+
+    Parametros do metodo mongoose.model()
+    1 - nome do modelo(sempre igual o nome do arquivo)
+    2 - estrutura(esquema) do modelo
+    3 - nome da coleção(collection) em que os objetos criados
+    modelo serão armazenados no MongoDB
+*/
+
+MSFIDOCredentialAssertion.exports = mongoose.model('Curso', esquema, 'cursos')
